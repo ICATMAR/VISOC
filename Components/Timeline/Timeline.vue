423c7d4c0fa9d53b35975770dadc9cba2314898a
Gerard Llorach <gerard.llorach@gmail.com>
2026-03-30 17:41:38 +0200

    added switch timeline to full timeline button

--- Components/Timeline/Timeline.vue
+++ Components/Timeline/Timeline.vue
@@ -2,19 +2,49 @@
   <span class="bottom-section">
     <!-- Timeline -->
     <div class="timeline-container">
-      
+
       <div class="timeline-content">
-        <!-- Play button -->
-        <button class="play-button"><span class="fa fa-play"></span></button>
+
+        <div class="vertical">
+          <!-- Swith to full timeline -->
+          <Transition name="scale-fade" mode="out-in">
+            <button class="full-timeline-switch" key="fulltime" v-if="!isFullTimeline" @click="isFullTimeline = !isFullTimeline">2018 - now</button>
+            <button class="full-timeline-switch" key="lastweek" v-else @click="isFullTimeline = !isFullTimeline">Last week</button>
+          </Transition>
+          <!-- Play button -->
+          <button class="play-button"><span class="fa fa-play"></span></button>
+        </div>
+
         <!-- Timeline -->
         <div class="timeline">
-        timeline
+          timeline from now to X days ago
         </div>
       </div>
 
     </div>
   </span>
 </template>
+
+
+<script>
+
+export default {
+  name: "Timeline",
+  created() {
+    
+  },
+  mounted() {
+  },
+  data (){
+    return {
+      isFullTimeline: false,
+    }
+  },
+  methods: {
+    //onclick: function(e){},
+  },
+}
+</script>
 
 
 
@@ -46,9 +76,30 @@
   align-items: flex-end;
 }
 
-.play-button {
-  margin-right: 15px;
+.full-timeline-switch {
+  margin-left: -16px;
   margin-bottom: 8px;
+
+  width: 50px;
+  height: 50px;
+  font-size: x-small;
+  border-radius: 50%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.play-button {
+  margin-right: 8px;
+  margin-bottom: 8px;
+
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .timeline {
