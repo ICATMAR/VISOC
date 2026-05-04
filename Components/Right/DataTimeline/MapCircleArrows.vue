c69d593e71368d30d2037002192ff57de9a69dc1
Gerard Llorach <gerard.llorach@gmail.com>
2026-05-04 13:54:21 +0200

    working on circle with arrows

--- Components/Right/DataTimeline/MapCircleArrows.vue
+++ Components/Right/DataTimeline/MapCircleArrows.vue
@@ -1,21 +1,54 @@
 <template>
   <div class="map-arrows-circle">
     <div class="map-arrows-center"></div>
-    <div class="map-arrow" style="transform: rotate(0deg) translateX(40px)"></div>
+
+    <!-- Variable name -->
+    <div class="variableName" :style="{ rotate: direction }"><span>{{ $t('Wind') }}</span></div>
+    <!-- Variable value -->
+    <div class="variableValue horizontal" :style="{ rotate: direction }">
+      <div class="variableArrow"></div>
+      <span>15 kn</span>
+    </div>
   </div>
 </template>
 
+<script>
+
+export default {
+  name: "MapCircleArrows",
+  created() {
+    
+  },
+  mounted() {
+  },
+  data (){
+    return {
+      direction: '180deg'
+    }
+  },
+  methods: {
+    //onclick: function(e){},
+  },
+  computed: {
+    
+  },
+  components: {
+    
+  }
+}
+
+</script>
 
 <style scoped>
 .map-arrows-circle {
   position: absolute;
   z-index: 1;
-  width: 80%;
-  height: 80%;
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
   border: 1px solid #ffffff6b;
-  top: 10%;
-  right: 10%;
+  top: 25%;
+  right: 25%;
 
   display: flex;
   align-items: center;
@@ -29,12 +62,38 @@
   border-radius: 50%;
 }
 
-.map-arrow {
-  width: 20px;
-  height: 5px;
-  background: #ffffffda;
+.variableName {
   position: absolute;
-  top: 50%;
-  left: 50%;
+  transform: translateX(calc(50% + 52px));
+  background: var(--darkBlue);
+  font-size: x-small;
+  padding-right: 6px;
+  padding-left: 2px;
+  border-radius: 0 5px 5px 0;
 }
+
+.variableValue {
+  position: absolute;
+  transform: translateX(calc(-50% + 49px));
+  background: var(--blue);
+  font-size: x-small;
+  padding-right: 2px;
+  padding-left: 4px;
+  border-radius: 0 4px 4px 0;
+}
+.variableValue > span {
+  z-index: 1;
+  /* Reverse the rotation so text stays horizontal */
+  transform: rotate(calc(-1 * var(--direction)));
+}
+
+.variableArrow {
+  position: absolute;
+  transform: translate(-80%) rotate(45deg);
+  background: var(--blue);
+  height: 10px;
+  width: 10px;
+  z-index: 0;
+}
+
 </style>
